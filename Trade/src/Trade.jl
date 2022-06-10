ebd59e95eb1ab3fd51ecf693d98a87c5d65e4e6f
tcae <tcae@posteo.nl>
2022-06-10 16:29:54 +0200

    Trade loop bug fixing

--- Trade/src/Trade.jl
+++ Trade/src/Trade.jl
@@ -11,7 +11,7 @@ All history data will be collected but a fixed subset **`historysubset`** will b
 """
 module Trade
 
-using Dates, DataFrames, JSON
+using Dates, DataFrames, JSON, Profile
 using EnvConfig, Ohlcv, Classify, CryptoXch, Assets, Features
 
 @enum OrderType buylongmarket buylonglimit selllongmarket selllonglimit
@@ -69,8 +69,9 @@ function preparetradecache(backtest)
         if EnvConfig.configmode == EnvConfig.test
             initialperiod = Dates.Minute(3 * Features.requiredminutes)
             # 3*requiredminutes for 1*  to calc features, 1* to build trade history, 1* to check Trade loop
-    else
-            initialperiod = Dates.Year(4)
+        else
+            initialperiod = Dates.Day(4)
+            # initialperiod = Dates.Year(4)
         end
         enddt = DateTime("2022-04-02T01:00:00")  # fix to get reproducible results
     else
@@ -88,7 +89,7 @@ function preparetradecache(backtest)
         Ohlcv.read!(ohlcv)
         origlen = size(ohlcv.df, 1)
         ohlcv.df = ohlcv.df[enddt .>= ohlcv.df.opentime .>= startdt, :]
-        println("cutting ohlcv from $origlen to $(size(ohlcv.df)) minutes")
+        println("cutting $base ohlcv from $origlen to $(size(ohlcv.df)) minutes")
         # CryptoXch.cryptoupdate!(ohlcv, startdt, enddt)  # not required because loadassets will already update
         if size(Ohlcv.dataframe(ohlcv), 1) < Features.requiredminutes
             @warn "insufficient ohlcv data returned for" base receivedminutes=size(Ohlcv.dataframe(ohlcv), 1) requiredminutes=Features.requiredminutes
@@ -186,7 +187,7 @@ function reportliquidity(cache, base)
     end
 end
 
-function closeorder!(cache, order, orderix, side, status)
+function closeorder!(cache, order, side, status)
     if cache.backtest
         if status == "FILLED"
             order = (;order..., executedQty=order.origQty)
@@ -201,8 +202,8 @@ function closeorder!(cache, order, orderix, side, status)
     df = Ohlcv.dataframe(cache.bd[order.base].features.ohlcv)
     opentime = df[!, :opentime]
     timeix = cache.bd[order.base].lastix
-    @info "close $side order #$(order.orderId) of $(order.origQty) $(order.base) (executed $(order.executedQty) $(order.base)) because $status at $(round(order.price;digits=3))USDT on ix:$(timeix) / $(EnvConfig.timestr(opentime[timeix]))"
-    reportliquidity(cache, order.base)
+    # @info "close $side order #$(order.orderId) of $(order.origQty) $(order.base) (executed $(order.executedQty) $(order.base)) because $status at $(round(order.price;digits=3))USDT on ix:$(timeix) / $(EnvConfig.timestr(opentime[timeix]))"
+    # reportliquidity(cache, order.base)
     order = (;order..., status = status)
     # order.status = status
     executedusdt = CryptoXch.floorbase("usdt", order.executedQty * order.price)
@@ -229,9 +230,11 @@ function closeorder!(cache, order, orderix, side, status)
         cache.bd[order.base].assetfree += order.origQty - order.executedQty
         cache.usdtfree += CryptoXch.floorbase("usdt", executedusdt * (1 - tradingfee)) #! TODO minus fee or fee is deducted from BNB
     end
-    deleteat!(cache.openorders, orderix)
+    cache.openorders = filter(row -> !(row.orderId == order.orderId), cache.openorders)
     push!(cache.orderlog, order)
     Classify.deletetradechanceoforder!(cache.tradechances, order.orderId)
+    totalusdt, _, _ = totalusdtliquidity(cache)
+    @info "close $side order #$(order.orderId) of $(order.origQty) $(order.base) (executed $(order.executedQty) $(order.base)) because $status at $(round(order.price;digits=3))USDT on ix:$(timeix) / $(EnvConfig.timestr(opentime[timeix]))  new total USDT = $totalusdt"
 end
 
 function buyqty(cache, base)
@@ -248,6 +251,9 @@ function neworder!(cache, base, price, qty, side, status, tc)
     qty = CryptoXch.floorbase(base, qty)  # see also minimum order granularity of xchange
     if cache.backtest
         orderid = Dates.value(convert(Millisecond, Dates.now())) # unique id in backtest
+        while !isnothing(Classify.tradechanceoforder(cache.tradechances, orderid))
+            orderid = Dates.value(convert(Millisecond, Dates.now())) # unique id in backtest
+        end
         order = (
             base = base,
             orderId = orderid, # unique id in backtest
@@ -266,7 +272,7 @@ function neworder!(cache, base, price, qty, side, status, tc)
         # use create order response to fill order record
         # check balances and whether it matches with fills of transactions including fees
     end
-    reportliquidity(cache, base)
+    # reportliquidity(cache, base)
     priceusdt = CryptoXch.roundbase("usdt", order.price)
     @info "open $(order.side) order #$(order.orderId) of $(order.origQty) $(order.base) because $(order.status) at $priceusdt USDT tc: $tc"
     if side == "BUY"
@@ -321,7 +327,7 @@ to be considered:
 """
 function trade!(cache)
     # TODO update TradeLog -> append to csv
-    for (orderix, order) in enumerate(copy.(eachrow(cache.openorders)))
+    for order in copy.(eachrow(cache.openorders))
         tc = Classify.tradechanceoforder(cache.tradechances, order.orderId)
         if isnothing(tc)
             @warn "no tradechance found for $(order.side) order #$(order.orderId)" tc
@@ -330,9 +336,9 @@ function trade!(cache)
         if order.side == "SELL"
             df = ohlcvdf(cache, order.base)
             if df.high[cache.bd[order.base].lastix] > order.price
-                closeorder!(cache, order, orderix, "SELL", "FILLED")
+                closeorder!(cache, order, "SELL", "FILLED")
             elseif significantsellpricechange(tc, order.price)  # including emergency sells
-                closeorder!(cache, order, orderix, "SELL", "CANCELED")
+                closeorder!(cache, order, "SELL", "CANCELED")
                 # buy order is closed, now issue a sell order
                 neworder!(cache, order.base, tc.sellprice, order.origQty, "SELL", "NEW", tc)
                 #? order.origQty remains unchanged?
@@ -341,7 +347,7 @@ function trade!(cache)
         if order.side == "BUY"
             df = ohlcvdf(cache, order.base)
             if df.low[cache.bd[order.base].lastix] < order.price
-                closeorder!(cache, order, orderix, "BUY", "FILLED")
+                closeorder!(cache, order, "BUY", "FILLED")
                 # buy order is closed, now issue a sell order
                 qty = cache.bd[order.base].assetfree
                 neworder!(cache, order.base, tc.sellprice, qty, "SELL", "NEW", tc)
@@ -350,15 +356,15 @@ function trade!(cache)
                 if order.base in keys(cache.tradechances.basedict)
                     # buy price not reached but other new buy chance available - hence, cancel old buy order and use new chance
                     newtc = cache.tradechances.basedict[order.base]
-                    closeorder!(cache, order, orderix, "BUY", "CANCELED")
+                    closeorder!(cache, order, "BUY", "CANCELED")
                     # buy order is closed, now issue a new buy order
                     neworder!(cache, order.base, newtc.buyprice, buyqty(cache, order.base), "BUY", "NEW", newtc)
                     Classify.deletenewbuychanceofbase!(cache.tradechances, order.base)
                 elseif tc.probability < 0.5
-                    closeorder!(cache, order, orderix, "BUY", "CANCELED")
+                    closeorder!(cache, order, "BUY", "CANCELED")
                     # buy order is closed because of low chance of buy success
                 elseif significantbuypricechange(tc, order.price)
-                    closeorder!(cache, order, orderix, "BUY", "CANCELED")
+                    closeorder!(cache, order, "BUY", "CANCELED")
                     # buy order is closed, now issue a new buy order with adapted price
                     neworder!(cache, order.base, tc.buyprice, buyqty(cache, order.base), "BUY", "NEW", tc)
                 end
@@ -380,6 +386,23 @@ end
 
 # end
 
+function coretradeloop(cache)
+    continuetrading = true
+    for base in keys(cache.bd)
+        continuetrading = appendmostrecent!(cache, base)
+        if continuetrading
+            cache.tradechances = cache.classify!(cache.tradechances, cache.bd[base].features, cache.bd[base].lastix)
+        else
+            assetrefresh = true
+            break
+        end
+    end
+    if continuetrading
+        trade!(cache)
+    end
+    return continuetrading
+end
+
 """
 **`tradeloop`** has to
 + get new exchange data (preferably non blocking)
@@ -390,23 +413,19 @@ end
 """
 function tradeloop(backtest=true)
     # TODO add hooks to enable coupling to the cockpit visualization
+    profileinit = false
+    Profile.clear()
     continuetrading = true
     while continuetrading
         cache = preparetradecache(backtest)
         assetrefresh = false
         refreshtimestamp = Dates.now(Dates.UTC)
+        println("start trading core loop")
         while !assetrefresh
-            for base in keys(cache.bd)
-                continuetrading = appendmostrecent!(cache, base)
-                if continuetrading
-                    cache.tradechances = cache.classify!(cache.tradechances, cache.bd[base].features, cache.bd[base].lastix)
-                else
-                    assetrefresh = true
-                    break
-                end
-            end
-            if continuetrading
-                trade!(cache)
+            if profileinit
+                @profile continuetrading = coretradeloop(cache)
+            else
+                continuetrading = coretradeloop(cache)
             end
         end
         # if !backtest && (Dates.now(Dates.UTC)-refreshtimestamp > Dates.Minute(12*60))
@@ -414,6 +433,8 @@ function tradeloop(backtest=true)
         # end
         reportliquidity(cache, nothing)
     end
+    println("finished trading core loop")
+    Profile.print()
 end
 
 end  # module
