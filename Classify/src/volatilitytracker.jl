cb5ae4c0d1ee4649ccc2bcda0194a39aab1d28dd
Torsten <dropincloud7.gmail.com>
2024-01-05 19:52:29 +0100

    volatility result in df and csv

--- Classify/src/volatilitytracker.jl
+++ Classify/src/volatilitytracker.jl
@@ -1,17 +1,19 @@
 
 module VolatilityTracker
 
-using DataFrames, Dates
+using DataFrames, Dates, Statistics, CSV
 using EnvConfig, TestOhlcv, Ohlcv, Features
+
+const VOLATILITYSTUDYFILE = "volatilitystudy.csv"
 
 emptytrackdf()::DataFrame = DataFrame(regr=[], direction=[], openix=[], closeix=[], tradelen=[], gain=[], gainthreshold=[], gap=[], dirregrwin=[])
 
-function trackregression(gainthreshold, dirregrwin, regrwindow, f2::Features.Features002)
+function trackregression(gainthreshold, dirregrwin, regrwindow, gap, f2::Features.Features002)
     df = emptytrackdf()
     piv = Ohlcv.dataframe(Features.ohlcv(f2)).pivot[f2.firstix:f2.lastix]
     callopenix = Int32[]
     putopenix = Int32[]
-    gap = 5  # 5 minute gap between trades
+    # gap = 5  # 5 minute gap between trades
     callcloseix = -gap
     putcloseix = -gap
 
@@ -47,21 +49,6 @@ function trackregression(gainthreshold, dirregrwin, regrwindow, f2::Features.Fea
             if !(relativedirgain > gainthreshold)
                 opentrades!(putopenix, ix)
             end
-            # while length(callopenix) > 0
-            #     # handle open call positions
-            #     gain = Ohlcv.relativegain(piv, callopenix[begin], ix)
-            #     tl = ix - callopenix[begin]
-            #     push!(df, (regr=regrwindow, direction="call", openix=callopenix[begin], closeix=ix, tradelen=tl, gain=gain, gainthreshold=gainthreshold, gap=gap, dirregrwin=dirregrwin))
-            #     callopenix = deleteat!(callopenix, 1)
-            #     if (relativedirgain > -gainthreshold) && (ix - callcloseix < gap)
-            #         callcloseix = ix
-            #         break
-            #     end
-            #     callcloseix = ix
-            # end
-            # if ((f2.regr[regrwindow].std[ix] / f2.regr[regrwindow].regry[ix]) > gainthreshold) && ((length(putopenix) == 0) || (ix - putopenix[end] >= gap))
-            #     push!(putopenix, ix)
-            # end
         elseif piv[ix] < f2.regr[regrwindow].regry[ix] - f2.regr[regrwindow].std[ix]
             if (relativedirgain > gainthreshold) || (ix - putcloseix >= gap)
                 putopenix = closetrades!(putopenix, ix, "put", (relativedirgain > gainthreshold))
@@ -70,66 +57,45 @@ function trackregression(gainthreshold, dirregrwin, regrwindow, f2::Features.Fea
             if !(relativedirgain < -gainthreshold)
                 opentrades!(callopenix, ix)
             end
-            # if (length(putopenix) > 0) && ((relativedirgain > gainthreshold) || (ix - putcloseix >= gap))
-            #     # handle open put positions
-            #     gain = -Ohlcv.relativegain(piv, putopenix[begin], ix)
-            #     tl = ix - putopenix[begin]
-            #     push!(df, (regr=regrwindow, direction="put", openix=putopenix[begin], closeix=ix, tradelen=tl, gain=gain, gainthreshold=gainthreshold, gap=gap, dirregrwin=dirregrwin))
-            #     putopenix = deleteat!(putopenix, 1)
-            #     putcloseix = ix
-            # end
-            # if ((f2.regr[regrwindow].std[ix] / f2.regr[regrwindow].regry[ix]) > gainthreshold) && ((length(callopenix) == 0) || (ix - callopenix[end] >= gap))
-            #     push!(callopenix, ix)
-            # end
         end
     end
     return df
 end
 
+function kpi(df, regr, dirregrwin, direction, gainthreshold, gap)
+    # println(describe(df))
+    if size(df, 1) > 0
+        gainvec = df[!, :gain]
+        tlvec = df[!, :tradelen]
+        cumgain = round(sum(gainvec)*100, digits=3)
+        count = length(gainvec)
+        meangain = round(mean(gainvec)*100, digits=3)
+        mediangain = round(median(gainvec)*100, digits=3)
+        mediantl = round(median(tlvec), digits=0)
+        maxtl = round(maximum(tlvec), digits=0)
+        return (regr=regr, dir=direction, dirregrwin=dirregrwin, gap=gap, gainthreshold=gainthreshold, cumgain=cumgain, meangain=meangain, mediangain=mediangain, count=count, mediantradelen=mediantl, maxtradelen=maxtl)
+    else
+        return (regr=regr, dir=direction, dirregrwin=dirregrwin, gap=gap, gainthreshold=gainthreshold, cumgain=0.0, meangain=0.0, mediangain=0.0, count=0, mediantradelen=0.0, maxtradelen=0.0)
+    end
+end
+
 function trackohlc(ohlcv::Ohlcv.OhlcvData)::DataFrame
-    df = emptytrackdf()
+    df = DataFrame()
     println("$(EnvConfig.now()) calculating F002 features")
     f2 = Features.Features002(ohlcv)
     rkeys = sort(collect(keys(f2.regr)))
-    gainthreshold = 0.005  # 0.5%
-    println("$(EnvConfig.now()) starting evaluation using $(gainthreshold*100)% as gain threshold")
     for kix in eachindex(rkeys)
         rw = rkeys[kix]
         println("\n$(EnvConfig.now()) processing regression window $rw")
         println(describe(DataFrame(grad=f2.regr[rw].grad, regry=f2.regr[rw].regry, std=f2.regr[rw].std), :all))
-        for gainthreshold in [0.005, 0.01, 0.02, 0.1]
-            if kix != firstindex(rkeys)
-                println("\n$(EnvConfig.now()) analyzing volatility performace of regression window $rw with gainthreshold=$gainthreshold")
-                kdf = trackregression(gainthreshold, rkeys[kix-1], rw, f2)
-                ckdf = filter(row -> row[:direction] == "call", kdf)
-                pkdf = filter(row -> row[:direction] == "put", kdf)
-                println(describe(ckdf))
-                println(describe(pkdf))
-                callgainvec = ckdf[!, :gain]
-                if length(callgainvec) > 0
-                    callgain = sum(callgainvec)
-                    callcount = count(i -> typeof(i)<:AbstractFloat, callgainvec)
-                    callavggain = round(callgain/callcount*100, digits=3)
-                else
-                    callgain = callavggain = 0.0
-                    callcount = 0
-                end
-                putgainvec = pkdf[!, :gain]
-                if length(callgainvec) > 0
-                    putgain = sum(putgainvec)
-                    putcount = count(i -> typeof(i)<:AbstractFloat, callgainvec)
-                    putavggain = round(putgain/putcount*100, digits=3)
-                else
-                    putgain = putavggain = 0.0
-                    putcount = 0
-                end
-                totalcount = callcount + putcount
-                totalavggain = totalcount > 0 ? round((callgain + putgain)/totalcount*100, digits=3) : 0.0
-                println("callgain=$callgain with $callcount trades = $callavggain% average gain per call trade")
-                println("putgain=$putgain with $putcount trades = $putavggain% average gain per put trade")
-                println("$totalavggain% average gain per trade \n")
-                if size(df, 1) > 0
-                    df = vcat(df, kdf)
+        if kix != firstindex(rkeys)
+            for gainthreshold in [0.005, 0.01, 0.02]
+                for gap in [2, 5]
+                    # println("$(EnvConfig.now()) analyzing volatility performace of regression window $rw, gainthreshold=$gainthreshold, gap=$gap")
+                    kdf = trackregression(gainthreshold, rkeys[kix-1], rw, gap, f2)
+                    ckdf = filter(row -> row[:direction] == "call", kdf)
+                    push!(df, kpi(filter(row -> row[:direction] == "call", kdf), rw, rkeys[kix-1], "call", gainthreshold, gap))
+                    push!(df, kpi(filter(row -> row[:direction] == "put", kdf), rw, rkeys[kix-1], "put", gainthreshold, gap))
                 end
             end
         end
@@ -144,6 +110,9 @@ function trackasset(base::String, startdt::Dates.DateTime=DateTime("2017-07-02T2
     subset!(ohlcv.df, :opentime => t -> startdt .<= t .<= enddt)
     println("loaded $ohlcv")
     df = trackohlc(ohlcv)
+    println(df)
+    pf = EnvConfig.logpath(VOLATILITYSTUDYFILE)
+    CSV.write(pf, df, decimal='.', delim=';')
     println("done")
 end
 
@@ -152,5 +121,5 @@ end  #of module
 using EnvConfig, Dates
 
 EnvConfig.init(production)
-# VolatilityTracker.trackasset("BTC")
-VolatilityTracker.trackasset("BTC", DateTime("2022-01-02T22:54:00"), Dates.Day(40))
+VolatilityTracker.trackasset("BTC")
+# VolatilityTracker.trackasset("BTC", DateTime("2023-01-02T22:54:00"), Dates.Day(40))
